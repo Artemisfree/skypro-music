75595dc4ca41d7c42871dc3c2a3c723b155c1a5f
Artem <nadtocheev.nadtocheev@gmail.com>
2024-07-26 12:52:12 +0300

    hw6. After review 1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import Playlist from '@/app/components/Playlist/Playlist'
 import App from '@/app/components/Main/Main'
+import { useRouter } from 'next/router'
 
 const HomePage: React.FC = () => {
+	const router = useRouter()
+	const { playlist } = router.query
+
+	let title = 'Треки'
+	let playlistId
+
+	switch (playlist) {
+		case 'indie':
+			title = 'Инди-заряд'
+			playlistId = 4
+			break
+		case 'hits':
+			title = '100 танцевальных хитов'
+			playlistId = 3
+			break
+		case 'daily':
+			title = 'Плейлист дня'
+			playlistId = 2
+			break
+		default:
+			title = 'Треки'
+			break
+	}
+
 	return (
-		<App title='Треки'>
-			<Playlist />
+		<App title={title}>
+			<Playlist playlistId={playlistId} />
 		</App>
 	)
 }
